1a7f658df3020fea17933f071711025a0bf85a78
Rob S <58272847+rob-ng15@users.noreply.github.com>
2023-07-22 10:13:51 +0100

    Optimisation of compressed instruction expansion
    In preparation for inclusion of zca and zcb extensions, combine as many similar compressed instructions as possible.

--- RV64/SOFTWARE/c/mc1-quake/WinQuake/quakedef.h
+++ RV64/SOFTWARE/c/mc1-quake/WinQuake/quakedef.h
@@ -8,7 +8,7 @@ of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 See the GNU General Public License for more details.
 
@@ -44,6 +44,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdio.h>
 #include <stdlib.h>
 #include <setjmp.h>
+
+
+#include <PAWSlibrary.h>
 
 #if defined(_WIN32) && !defined(WINDED)
 
