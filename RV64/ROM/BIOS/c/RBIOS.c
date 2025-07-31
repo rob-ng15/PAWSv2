dd0dcb4a9c0897fd03a91676a8d4a43607d1a2b0
Rob S <58272847+rob-ng15@users.noreply.github.com>
2025-07-31 09:38:34 +0100

    Tidy of CPU_*.si code
    Tidy up CPU_*.si units, use trackers where necessary.

--- RV64/ROM/BIOS/c/RBIOS.c
+++ RV64/ROM/BIOS/c/RBIOS.c
@@ -1,81 +1,32 @@
 #include "PAWS.h"
 #include "RAYTRACE.h"
-
-// DMA CONTROLLER
-void DMASTART( const void *restrict source, void *restrict destination, unsigned int count, unsigned short mode ) {
-    *DMASOURCE = (unsigned long)source;
-    *DMADEST = (unsigned long)destination;
-    *DMACOUNT = count;
-    *DMAMODE = mode;
-}
-
-// STANDARD C FUNCTIONS ( from @sylefeb mylibc )
-void *memset(void *destination, int val, size_t len) {
-    *DMASET = val;
-    DMASTART( (const void *restrict)DMASET, destination, len, DMA_SET_TO_M );
-    return destination;
-}
-void *memset32( void *restrict destination, int value, size_t count ) {
-    *DMASET32 = value; DMASTART( (const void *restrict)DMASET, destination, count, DMA_SET_TO_M );
-    return( destination );
-}
-
-void *memcpy( void *restrict destination, const void *restrict source, size_t count ) {
-    DMASTART( source, destination, count, DMA_CPY_M_TO_M );
-    return( destination );
-}
-
-// INTERNAL FUNCTION - WAIT FOR THE GPU TO FINISH THE LAST COMMAND
-void wait_gpu( void ) {
-    while( *GPU_STATUS );
-}
-
-// DRAW A FILLED RECTANGLE from (x1,y1) to (x2,y2) in colour
-void gpu_rectangle( unsigned char colour, short x1, short y1, short x2, short y2 ) {
-    *GPU_COLOUR = colour;
-    *GPU_X = x1;
-    *GPU_Y = y1;
-    *GPU_PARAM0 = x2;
-    *GPU_PARAM1 = y2;
-
-    wait_gpu();
-    *GPU_WRITE = 3;
-}
-
-// CLEAR THE BITMAP by drawing a transparent rectangle from (0,0) to (639,479) and resetting the bitamp scroll position
-void gpu_cs( void ) {
-    wait_gpu();
-    gpu_rectangle( 64, 0, 0, 319, 239 );
-}
-
-// CLEAR THE CHARACTER MAP
-void tpu_cs( void ) {
-    memset32( ( void *)0x1000000, ( 64 << 16 ), 4800 * 4 );
-}
+#include "PAWS_BIOS_LIBRARY.h"
 
 void reset_display( void ) {
+    *AUDIO_DMA_L_STATUS = 1; *AUDIO_DMA_R_STATUS = 1;
+    for( int i = 0; i < 6; i++ ) AUDIO_DURATION[ i ] = 0;
+
     *GPU_DITHERMODE = 0;
+    *CROP_LEFT = 0; *CROP_RIGHT = 319; *CROP_TOP = 0; *CROP_BOTTOM = 239;
     *FRAMEBUFFER_DRAW = 3; gpu_cs(); while( !*GPU_FINISHED );
-    *FRAMEBUFFER_DRAW = 1; *FRAMEBUFFER_DISPLAY = 1;
-    *SCREENMODE = 0; *COLOUR = 0;
-    tpu_cs();
-    *LOWER_TM_SCROLLWRAPCLEAR = 5;
-    *UPPER_TM_SCROLLWRAPCLEAR = 5;
-    for( unsigned short i = 0; i < 16; i++ ) {
-        LOWER_SPRITE_ACTIVE[i] = 0;
-        UPPER_SPRITE_ACTIVE[i] = 0;
+    *FRAMEBUFFER_DRAW = 1; *BITMAP_DISPLAY256 = 0;
+    *SCREENORDER = ( 3 << 0 ) | ( 1 << 4 ) | ( 4 << 8 ) | ( 8 << 12 ) | ( 9 << 16 ) | ( 10 << 20 ) | ( 11 << 24 );
+    *COLOUR = 0;
+    tpu_cs(); *TPU_LOREZ = 0;
+    *TM_LOREZ = 0;
+    for( int i = 0; i < 4; i++ ) {
+        tm_cs( i );
+        TM_SCROLLAMOUNT[i] = 1;
     }
+    for( int i = 0; i < 63; i++ ) SPRITE_ACTIVE[i] = 0;
 }
 
 extern int _bss_start, _bss_end;
 
 void main( void ) {
-    // STOP SMT
-    *SMTSTATUS = 0;
-
     // CLEAR MEMORY
     memset( &_bss_start, 0, &_bss_end - &_bss_start );
-    memcpy( ( void *)(long)*RAMBASE, &array[0], 6568 );
+    memcpy( ( void *)(long)*RAMBASE, (void *)array, sizeof(array) );
 
     // RESET THE DISPLAY
     reset_display();
